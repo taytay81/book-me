33be7d3828b6c5c8d92765e573e966f17e31ad41
unknown <tayssire@hotmail.com>
2020-02-10 17:49:58 +0100

    modification added to the add book view

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,37 +27,92 @@ const getBooks = async isbn => {
     );
   }
 };
+
+function retrieveTheBestBook(data) {
+  for (let i = 0; i < data.totalItems; i++) {
+    if (data.items[i].saleInfo.saleability === "FOR_SALE") {
+      return data.items[i];
+    }
+  }
+  return data.items[0];
+}
+function calculatePoints(price, page_count, state) {
+  var finalPoints = 10;
+  const maxprice = 10;
+  const maxpage = 10;
+  finalPoints = finalPoints + (price / 5 + page_count / 10);
+  console.log("xxxxxx", finalPoints);
+  if (state == "likenew") {
+    finalPoints = finalPoints + 30;
+  } else if (state == "verygood") {
+    finalPoints = finalPoints + 21;
+  } else if (state == "good") {
+    finalPoints = finalPoints + 9;
+  } else if (state == "acceptable") {
+    finalPoints = finalPoints + 5;
+  }
+  printHtmlPoints(Math.round(finalPoints));
+}
+function printHtmlPoints(points) {
+  document.getElementById("points_id").value = points;
+}
 //function that get the result of the googleapi and prin it inside the form
 function printHtmlBook(data) {
   if (data.totalItems > 0) {
-    const mybook = data.items[0].volumeInfo;
+    const mybook = retrieveTheBestBook(data);
     const isbn = document.getElementById("isbn_id");
     const title = document.getElementById("title_id");
     const img = document.getElementById("img_id");
     const authors = document.getElementById("author_id");
-    const publishers = document.getElementById("publisher_id");
+    const publisher = document.getElementById("publisher_id");
     const price = document.getElementById("price_id");
     const description = document.getElementById("description_id");
     const category = document.getElementById("category_id");
     const publishedDate = document.getElementById("published_date_id");
-    const page = document.getElementById("page_count_id");
-    const points = document.getElementById("points_id");
+    const pages = document.getElementById("page_count_id");
 
-    title.value = mybook.title;
-    isbn.value = mybook.industryIdentifiers[0].identifier;
-    img.src = extractThumbnail(mybook);
-    //image_value_id.value = mybook.imageLinks.thumbnail;
+    const language = document.getElementById("language_id");
 
-    if (mybook.authors.length > 1) {
+    title.value = `"${mybook.volumeInfo.title}"`;
+    title.style.width = mybook.volumeInfo.title.length + "ch";
+
+    isbn.value = mybook.volumeInfo.industryIdentifiers[0].identifier;
+    isbn.style.width = isbn.value.length + 2 + "ch";
+
+    img.src = extractThumbnail(mybook.volumeInfo);
+    image_value_id.value = extractThumbnail(mybook.volumeInfo);
+
+    publishedDate.value = mybook.volumeInfo.publishedDate.substring(0, 10);
+    publishedDate.style.width = publishedDate.value.length + "ch";
+
+    pages.value = mybook.volumeInfo.pageCount;
+    pages.style.width = pages.value.length + 2 + "ch";
+
+    category.value = mybook.volumeInfo.categories[0];
+    category.style.width = category.value.length + "ch";
+
+    language.value = mybook.volumeInfo.language;
+    language.style.width = language.value.length + "ch";
+
+    price.value = mybook.saleInfo.listPrice.amount;
+    price.style.width = price.value.length + 2 + "ch";
+
+    description.value = mybook.volumeInfo.description;
+    description.style.width = description.value.length + "ch";
+    if (mybook.volumeInfo.authors.length > 1) {
       const authorsSection = document.getElementById("authors_section");
       var authorsInnerhtml = `<label for="">Authors</label>`;
-      for (let i = 0; i < mybook.authors.length; i++) {
-        authorsInnerhtml += `<input id="author_id" type="text" value=" ${mybook.authors[i]} " name="author"></input>`;
+      for (let i = 0; i < mybook.volumeInfo.authors.length; i++) {
+        authorsInnerhtml += `<input id="author_id" type="text" value=" ${mybook.volumeInfo.authors[i]} " name="author"></input>`;
       }
       authorsSection.innerHTML = authorsInnerhtml;
     } else {
-      authors.value = mybook.authors[0];
+      authors.value = mybook.volumeInfo.authors[0];
     }
+    authors.style.width = authors.value.length + "ch";
+
+    publisher.value = mybook.volumeInfo.publisher;
+    publisher.style.width = publisher.value.length + 1 + "ch";
   } else {
     const message = document.getElementById("messageFromApi");
     message.innerHTML =
@@ -77,7 +132,18 @@ window.addEventListener("load", () => {
       event.preventDefault();
       console.log("salut");
       var test = getBooks(searchBooks.value);
-      //console.log(returndata);
+    });
+
+  document
+    .getElementById("state_id")
+    .addEventListener("change", function(event) {
+      event.preventDefault();
+      console.log("hey clicked on the dropdownlist");
+      const price = document.getElementById("price_id").value;
+      const page_count = document.getElementById("page_count_id").value;
+      const state = document.getElementById("state_id").value;
+      console.log(price, page_count, state);
+      calculatePoints(price, page_count, state);
     });
 });
 
